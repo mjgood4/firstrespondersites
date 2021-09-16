a7f8f893305d9688bfbd39131e91878c467b712d
ddd00a <mr.anderson.1725@gmail.com>
2021-09-15 19:21:52 -0500

    added nearest distance

--- research/eda/manderson334/features/distance_from_station.py
+++ research/eda/manderson334/features/distance_from_station.py
@@ -34,13 +34,32 @@ def compute_incident_distances(fac_df, inc_coords_df):
         if counter_i % 10000 == 0: print(f"\t{counter_i}/{inc_coords_df.shape[0]}")        
         # calculate euclidean distance
         row_dist = np.sqrt(np.sum(np.power((fac_dist - df_row.to_numpy()), 2), axis=1))
-        # return a transposed array
         return pd.Series(row_dist)
 
     incident_dist = inc_coords_df[['lat', 'lon']].apply(compute_row_dist, axis=1)
     incident_dist.columns = fac_df.common_name.tolist()
     
-    return incident_dist
+    return pd.concat([inc_coords_df, incident_dist], axis=1)
+
+def compute_nearest_distance(fac_df, inc_coords_df):
+    
+    fac_dist = fac_df[['latitude', 'longitude']].to_numpy(dtype=float)
+    counter_i = 0
+
+    def compute_row_dist(df_row):
+        # print progress
+        nonlocal counter_i
+        counter_i = counter_i + 1
+        if counter_i % 10000 == 0: print(f"\t{counter_i}/{inc_coords_df.shape[0]}")        
+        # calculate euclidean distance
+        row_dist = np.min(np.sqrt(np.sum(np.power((fac_dist - df_row), 2), axis=1)))
+        return row_dist
+
+    incident_dist = inc_coords_df[['lat', 'lon']].apply(compute_row_dist, axis=1, raw=True, result_type="reduce")  
+    incident_dist = pd.DataFrame(incident_dist, index=['nearest_station'])  
+    
+    return pd.concat([inc_coords_df, incident_dist], axis=1)
+
 
 if __name__ == "__main__":
     print("reading facilities")
@@ -51,4 +70,8 @@ if __name__ == "__main__":
     dist_df = compute_incident_distances(fac_df, inc_coords_df)
     print("writing csv...")
     dist_df.to_csv("/data/incident_distances.csv")
+    print("computing nearest distances")
+    dist_df = compute_nearest_distance(fac_df, inc_coords_df)
+    print("writing csv...")
+    dist_df.to_csv("/data/incident_nearest_distances.csv")
     print("done")
